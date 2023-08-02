9ebef7b30164c4a55a6292cef1351e7f49d719f9
June woo Kang <133371686+Jnuary@users.noreply.github.com>
2023-08-02 16:54:45 +0900

    origin file (only name)

--- restFront.js
+++ restFront.js
@@ -1,34 +1,31 @@
-async function getUser() {
+async function getUser() { // 로딩 시 사용자 가져오는 함수
   try {
     const res = await axios.get('/users');
     const users = res.data;
     const list = document.getElementById('list');
     list.innerHTML = '';
-
+    // 사용자마다 반복적으로 화면 표시 및 이벤트 연결
     Object.keys(users).map(function (key) {
       const userDiv = document.createElement('div');
       const span = document.createElement('span');
-      span.textContent = `${users[key].name} (${users[key].age}세)`; // 이름과 나이를 함께 출력합니다.
-
+      span.textContent = users[key];
       const edit = document.createElement('button');
       edit.textContent = '수정';
-      edit.addEventListener('click', async () => {
+      edit.addEventListener('click', async () => { // 수정 버튼 클릭
         const name = prompt('바꿀 이름을 입력하세요');
-        const age = prompt('나이를 입력하세요');
-        if (!name || !age) {
-          return alert('이름과 나이를 반드시 입력하셔야 합니다.');
+        if (!name) {
+          return alert('이름을 반드시 입력하셔야 합니다');
         }
         try {
-          await axios.put('/user/' + key, { name, age });
+          await axios.put('/user/' + key, { name });
           getUser();
         } catch (err) {
           console.error(err);
         }
       });
-
       const remove = document.createElement('button');
       remove.textContent = '삭제';
-      remove.addEventListener('click', async () => {
+      remove.addEventListener('click', async () => { // 삭제 버튼 클릭
         try {
           await axios.delete('/user/' + key);
           getUser();
@@ -36,32 +33,30 @@ async function getUser() {
           console.error(err);
         }
       });
-
       userDiv.appendChild(span);
       userDiv.appendChild(edit);
       userDiv.appendChild(remove);
       list.appendChild(userDiv);
+      console.log(res.data);
     });
   } catch (err) {
     console.error(err);
   }
-}//뭐?
+}
 
-window.onload = getUser;
-
+window.onload = getUser; // 화면 로딩 시 getUser 호출
+// 폼 제출(submit) 시 실행
 document.getElementById('form').addEventListener('submit', async (e) => {
   e.preventDefault();
   const name = e.target.username.value;
-  const age = e.target.age.value; // 폼에 나이 입력란을 추가합니다.
-  if (!name || !age) {
-    return alert('이름과 나이를 입력하세요');
+  if (!name) {
+    return alert('이름을 입력하세요');
   }
   try {
-    await axios.post('/user', { name, age }); // 나이 정보도 함께 전송합니다.
+    await axios.post('/user', { name });
     getUser();
   } catch (err) {
     console.error(err);
   }
   e.target.username.value = '';
-  e.target.age.value = '';
 });
--- restServer.js
+++ restServer.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
 
-const users = {};
+const users = {}; // 데이터 저장용
 
 http.createServer(async (req, res) => {
   try {
@@ -19,26 +19,28 @@ http.createServer(async (req, res) => {
         res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
         return res.end(JSON.stringify(users));
       }
-
+      // /도 /about도 /users도 아니면
       try {
         const data = await fs.readFile(path.join(__dirname, req.url));
         return res.end(data);
       } catch (err) {
-        res.writeHead(404);
-        return res.end('NOT FOUND');
+        // 주소에 해당하는 라우트를 못 찾았다는 404 Not Found error 발생
       }
     } else if (req.method === 'POST') {
       if (req.url === '/user') {
         let body = '';
+        // 요청의 body를 stream 형식으로 받음
         req.on('data', (data) => {
           body += data;
         });
+        // 요청의 body를 다 받은 후 실행됨
         return req.on('end', () => {
-          const { name, age } = JSON.parse(body); // 나이 정보도 받아옵니다.
+          console.log('POST 본문(Body):', body);
+          const { name } = JSON.parse(body);
           const id = Date.now();
-          users[id] = { name, age }; // 나이 정보를 포함한 사용자 정보를 저장합니다.
+          users[id] = name;
           res.writeHead(201, { 'Content-Type': 'text/plain; charset=utf-8' });
-          res.end(JSON.stringify({ message: '등록 성공' }));
+          res.end('등록 성공');
         });
       }
     } else if (req.method === 'PUT') {
@@ -49,7 +51,8 @@ http.createServer(async (req, res) => {
           body += data;
         });
         return req.on('end', () => {
-          users[key] = JSON.parse(body);
+          console.log('PUT 본문(Body):', body);
+          users[key] = JSON.parse(body).name;
           res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
           return res.end(JSON.stringify(users));
         });
@@ -62,7 +65,6 @@ http.createServer(async (req, res) => {
         return res.end(JSON.stringify(users));
       }
     }
-
     res.writeHead(404);
     return res.end('NOT FOUND');
   } catch (err) {
@@ -71,6 +73,6 @@ http.createServer(async (req, res) => {
     res.end(err.message);
   }
 })
-  .listen(8083, () => {
-    console.log('8083번 포트에서 서버 대기 중입니다');
+  .listen(8082, () => {
+    console.log('8082번 포트에서 서버 대기 중입니다');
   });
